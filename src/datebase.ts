7741e34b851bd71365c4f94417c96fd2876d108b
xiomaraCortes <55770691+xiomaraCortes@users.noreply.github.com>
2022-05-18 14:20:27 -0500

    db

--- src/datebase.ts
+++ src/datebase.ts
@@ -2,10 +2,10 @@ import sql from "mssql";
 
 export async function conecion(){
     var config = {
-        server: 'localhost',
-        database:'LovePet',
-        user:'sa', 
-        password:'_falso00',
+        server: 'us-cdbr-east-05.cleardb.net',
+        database:'heroku_eba584c843cbe15',
+        user:'b31f987925bf35', 
+        password:'9e84a48c',
         
         // port:1433,
         drive:'tedios',
